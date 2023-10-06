d9af5dc7a42232dd5c6f42921f1b686b4e716e6a
JonathanSerrano13 <145078783+JonathanSerrano13@users.noreply.github.com>
2023-10-06 08:57:57 -0600

    BatallaPokemon

---
+++ Pokemon_Herencia/BatallaPokemon.java
@@ -0,0 +1,14 @@
+package pokemon;
+
+/**
+ *
+ * @author Alumno
+ */
+public class BatallaPokemon {
+
+    public static void main(String[] args) {
+ 
+        PokemonElectrico Pikachu=new PokemonElectrico("Pikachu",10);
+        PokemonFantasmma Gastly=new PokemonFantasma("Gastly",10);    }
+    
+}
