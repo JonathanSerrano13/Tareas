9faef381a4907ccb20f7e11c3d048a33fb106bb3
JonathanSerrano13 <145078783+JonathanSerrano13@users.noreply.github.com>
2023-10-06 08:56:23 -0600

    PokemonElectrico

---
+++ Pokemon_Herencia/PokemonElectrico.java
@@ -0,0 +1,11 @@
+package pokemon;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PokemonElectrico extends Pokemon {
+        public PokemonElectrico(String nombre, int nivel) {
+        super (Pikachu, "Electrico", 10);
+        }
+    }
