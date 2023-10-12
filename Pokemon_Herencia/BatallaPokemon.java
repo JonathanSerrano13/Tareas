04f161bfafb1a2e938c4d65edbb58c8428c9e291
JonathanSerrano13 <145078783+JonathanSerrano13@users.noreply.github.com>
2023-10-11 22:06:32 -0600

    BatallaPokemon.java

--- Pokemon_Herencia/BatallaPokemon.java
+++ Pokemon_Herencia/BatallaPokemon.java
@@ -1,14 +1,14 @@
-package pokemon;
-
-/**
- *
- * @author Alumno
- */
 public class BatallaPokemon {
 
     public static void main(String[] args) {
- 
-        PokemonElectrico Pikachu=new PokemonElectrico("Pikachu",10);
-        PokemonFantasmma Gastly=new PokemonFantasma("Gastly",10);    }
-    
+        PokemonElectrico pokemonElectrico=new PokemonElectrico("Pikachu","PokemonElectrico",10);
+        PokemonFantasma pokemonFantasma=new PokemonFantasma("Gengar" ,"PokemonFantasma",10);    
+        pokemonElectrico.atacar("Rayitos relampagos",pokemonFantasma);
+        pokemonFantasma.recibirAtaque("Rayitos relampagos");
+        System.out.println("Hp = "+pokemonFantasma.getHp());
+        pokemonFantasma.atacar("Sombra espectral",pokemonElectrico);
+        pokemonElectrico.recibirAtaque("Sombra espectral");
+        System.out.println("Hp = "+pokemonElectrico.getHp());
+
+    }
 }
