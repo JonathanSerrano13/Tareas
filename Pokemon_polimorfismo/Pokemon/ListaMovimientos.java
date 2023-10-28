fdb0ec4346c4a23e0d65d1bbfac7935bd358dc65
JonathanSerrano13 <145078783+JonathanSerrano13@users.noreply.github.com>
2023-10-27 22:02:16 -0600

    ListaMovimientos

--- Pokemon_polimorfismo/Pokemon/ListaMovimientos.java
+++ Pokemon_polimorfismo/Pokemon/ListaMovimientos.java
@@ -6,22 +6,16 @@ public class ListaMovimientos{
     private ArrayList<Movimiento>movimientos=new ArrayList<>();
 
     public ListaMovimientos(){
-        //Movimientos de tipo electrico
-        movimientos.add(new Movimiento("Impactrueno",40,Tipo.ELECTRICO,30));
-        movimientos.add(new Movimiento("Bola voltio",60,Tipo.ELECTRICO,10));
-        movimientos.add(new Movimiento("Ataque rapido",40,Tipo.ELECTRICO,30));
-        movimientos.add(new Movimiento("Chispa",65,Tipo.ELECTRICO,20));
-        //Movimientos de tipo fantasma
-        movimientos.add(new Movimiento("Puño sombra",60,Tipo.FANTASMA,20));
-        movimientos.add(new Movimiento("Lengüetazo",30,Tipo.FANTASMA,30));
-        movimientos.add(new Movimiento("Golpe bajo",70,Tipo.FANTASMA,5));
-        movimientos.add(new Movimiento("Vendetta",50,Tipo.FANTASMA,35));
-        //Movimientos de tipo siniestro
-        movimientos.add(new Movimiento("Picotazo",35,Tipo.SINIESTRO,35));
-        movimientos.add(new Movimiento("Ataque Ala",60,Tipo.SINIESTRO,35));
-        //Movimientos de tipo agua
-        movimientos.add(new Movimiento("Placaje",40,Tipo.AGUA,35));
-        movimientos.add(new Movimiento("Mordisco",60,Tipo.AGUA,25));
+      //Movimientos de tipo siniestro
+        movimientos.add(new Movimiento("Picotazo",35,Tipo.SINIESTRO,35, Clase.FISICO));
+        movimientos.add(new Movimiento("Golpe bajo",70,Tipo.SINIESTRO,5, Clase.FISICO));
+        movimientos.add(new Movimiento("Ataque Ala",60,Tipo.SINIESTRO,35, Clase.FISICO));
+        movimientos.add(new Movimiento("Finta",60,Tipo.SINIESTRO,20, Clase.FISICO));
+        //Movimientos de tipo DragonFantasma
+        movimientos.add(new Movimiento("Maldición",40,Tipo.DRAGONFANTASMA,35, Clase.FISICO));
+        movimientos.add(new Movimiento("Impresionar",60,Tipo.DRAGONFANTASMA,25, Clase.FISICO));
+        movimientos.add(new Movimiento("Bola Sombra",60,Tipo.DRAGONFANTASMA,35, Clase.ESPECIAL));
+        movimientos.add(new Movimiento("Ataque Ala",60,Tipo.DRAGONFANTASMA,35, Clase.FISICO));
     }
 
     //Metodo que devuelve el movimiento uscando el nombre
