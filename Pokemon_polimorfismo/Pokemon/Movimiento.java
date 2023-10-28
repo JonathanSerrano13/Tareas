8ff560a16baeeb2a52962c096367489c0202a6f9
JonathanSerrano13 <145078783+JonathanSerrano13@users.noreply.github.com>
2023-10-27 22:03:13 -0600

    Movimiento

--- Pokemon_polimorfismo/Pokemon/Movimiento.java
+++ Pokemon_polimorfismo/Pokemon/Movimiento.java
@@ -4,16 +4,21 @@ public class Movimiento{
     private String nombre;
     private int puntosdeAtaque,pp;
     private Tipo tipo;
-    private ataqueTipo Tipo;
+    private Clase ataqueTipo;
 
 
     
-    public Movimiento(String nombre, int puntosdeAtaque,Tipo tipo, int pp) {
+  
+    public Movimiento(String nombre, int puntosdeAtaque,Tipo tipo, int pp, Clase ataqueTipo) {
         this.nombre = nombre;
         this.puntosdeAtaque = puntosdeAtaque;
         this.tipo = tipo;
         this.pp = pp;
+        this.ataqueTipo = ataqueTipo;
+        
     }
+
+    
 
     public String getNombre() {
         return nombre;
@@ -30,5 +35,9 @@ public class Movimiento{
     public void setPp(int pp) {
         this.pp = pp;
     }
-    
-}
+      public pokemon.Clase getClase() {
+        return ataqueTipo;
+    }
+
+
+} 
